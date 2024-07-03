46121d8b64dcff4e5f67934f3dddf3f42abc5272
Kacper Bak <kacper@quantstamp.com>
2024-07-03 21:13:49 +0300

    lowercase addresses

--- src/handlers/SY.ts
+++ src/handlers/SY.ts
@@ -24,7 +24,7 @@ async function processAccount(account: string, ctx: ERC20Context) {
   if (isPendleAddress(account)) return;
   const timestamp = getUnixTimestamp(ctx.timestamp);
   const ts : bigint = BigInt(timestamp).valueOf();
-  const accountId = account + POINT_SOURCE_SY;
+  const accountId = account.toLowerCase() + POINT_SOURCE_SY;
   const snapshot = await ctx.store.get(AccountSnapshot, accountId);
 
   if (snapshot && snapshot.lastUpdatedAt < ts) {
